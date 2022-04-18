afad2bd539b502917a6d36c4da82e01c6663fd7e
winan <winand1990@live.nl>
2022-04-18 22:46:59 +0200

    It works woop

--- RendererTest/Program.cs
+++ RendererTest/Program.cs
@@ -1,82 +1,152 @@
+using System.Diagnostics;
 using Bgfx;
+using Silk.NET.Core.Contexts;
 using Silk.NET.GLFW;
 
 namespace RendererTest;
 
 internal static unsafe class Program
 {
+    private static readonly Glfw Glfw = Glfw.GetApi();
+
     private static WindowHandle* _windowHandle;
 
     internal static void Main()
     {
-        EntryGlfw.CurrentGlfw.SetErrorCallback(ErrorCallback);
+        Glfw.SetErrorCallback(ErrorCallback);
 
-        if (!EntryGlfw.CurrentGlfw.Init())
-        {
-            throw new GlfwException("GLFW INITIALIZATION FAILED!");
-        }
+        if (!Glfw.Init()) throw new GlfwException("GLFW INITIALIZATION FAILED!");
 
-        EntryGlfw.CurrentGlfw.WindowHint(WindowHintClientApi.ClientApi, ClientApi.NoApi);
+        Glfw.WindowHint(WindowHintClientApi.ClientApi, ClientApi.NoApi);
 
-        _windowHandle = EntryGlfw.CurrentGlfw.CreateWindow(800, 600, "BGFX Test Window", null, null);
+        _windowHandle = Glfw.CreateWindow(800, 600, "BGFX Test Window", null, null);
 
         if (_windowHandle is null)
         {
-            EntryGlfw.CurrentGlfw.Terminate();
+            Glfw.Terminate();
 
             throw new GlfwException("GLFW WINDOW CREATION FAILED!");
         }
-        
-        var test = new Thread(Test);
-        
-        test.Start();
-    }
 
-    private static void Test()
-    {
-        var init = new bgfx.Init
+        var pd = GetPlatformData(_windowHandle);
+
+        bgfx.set_platform_data(&pd);
+
+        var init = new bgfx.Init();
+
+        bgfx.init_ctor(&init);
+
+        init.type = bgfx.RendererType.Count;
+        init.resolution = new bgfx.Resolution
         {
-            type = bgfx.RendererType.Count,
-            resolution = new bgfx.Resolution
-            {
-                format = bgfx.TextureFormat.BC1,
-                width = 800,
-                height = 600,
-                reset = (uint)bgfx.ResetFlags.Vsync,
-            },
+            width = 800,
+            height = 600,
+            reset = (uint) bgfx.ResetFlags.Vsync
         };
-
-        EntryGlfw.GlfwSetWindow(_windowHandle);
 
         bgfx.init(&init);
 
-        bgfx.set_debug((uint)bgfx.DebugFlags.Text);
+        bgfx.set_debug((uint) bgfx.DebugFlags.Text);
 
         bgfx.set_view_clear(0,
-            (ushort)(bgfx.ClearFlags.Color | bgfx.ClearFlags.Depth),
+            (ushort) (bgfx.ClearFlags.Color | bgfx.ClearFlags.Depth),
             0x303030ff,
             1.0f,
             0
         );
 
-        while (!EntryGlfw.CurrentGlfw.WindowShouldClose(_windowHandle))
+        while (!Glfw.WindowShouldClose(_windowHandle))
         {
             bgfx.set_view_rect(0, 0, 0, 800, 600);
 
             bgfx.touch(0);
 
-            bgfx.frame(false);
+            Debug.WriteLine("Help");
 
-            EntryGlfw.CurrentGlfw.PollEvents();
+            Glfw.PollEvents();
         }
-        
-        EntryGlfw.GlfwDestroyWindow(_windowHandle);
 
-        EntryGlfw.CurrentGlfw.Terminate();
+        GlfwDestroyWindow(_windowHandle);
+
+        Glfw.Terminate();
     }
 
     private static void ErrorCallback(ErrorCode error, string description)
     {
         throw new GlfwException($"GLFW ERROR: {error}, {description}");
+    }
+
+    public static bgfx.PlatformData GetPlatformData(WindowHandle* windowHandle)
+    {
+        var glfwNativeWindowHandle = GlfwNativeWindowHandle(windowHandle, out var display);
+
+        return new bgfx.PlatformData
+        {
+            ndt = display,
+            nwh = glfwNativeWindowHandle,
+            context = null,
+            backBuffer = null,
+            backBufferDS = null
+        };
+    }
+
+    public static void* GlfwNativeWindowHandle(WindowHandle* windowHandle, out void* display)
+    {
+        var nativeWindow = new GlfwNativeWindow(Glfw, windowHandle);
+
+        display = default;
+
+        var window = default(void*);
+
+        switch (nativeWindow.Kind - 1)
+        {
+            case NativeWindowFlags.Win32:
+                window = (void*) nativeWindow.Win32!.Value.Hwnd;
+                break;
+            case NativeWindowFlags.X11:
+                display = (void*) nativeWindow.X11!.Value.Display;
+                window = (void*) nativeWindow.X11!.Value.Window;
+                break;
+            case NativeWindowFlags.DirectFB:
+                break;
+            case NativeWindowFlags.Cocoa:
+                window = (void*) nativeWindow.Cocoa!.Value;
+                break;
+            case NativeWindowFlags.UIKit:
+                break;
+            case NativeWindowFlags.Wayland:
+                display = (void*) nativeWindow.Wayland!.Value.Display;
+                window = (void*) nativeWindow.Wayland!.Value.Surface;
+                break;
+            case NativeWindowFlags.WinRT:
+                break;
+            case NativeWindowFlags.Android:
+                break;
+            case NativeWindowFlags.Vivante:
+                break;
+            case NativeWindowFlags.OS2:
+                break;
+            case NativeWindowFlags.Haiku:
+                break;
+            case NativeWindowFlags.Glfw:
+                break;
+            case NativeWindowFlags.Sdl:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        if (window == default) 
+            throw new GlfwException("Could not get window handle pointer!");
+
+        return window;
+    }
+
+    public static void GlfwDestroyWindow(WindowHandle* windowHandle)
+    {
+        if (windowHandle == null)
+            return;
+
+        Glfw.DestroyWindow(windowHandle);
     }
 }
---
+++ RendererTest/lib/BGFX/bgfx_dllname.cs
@@ -0,0 +1,6 @@
+namespace Bgfx;
+
+public static partial class bgfx
+{
+    const string DllName = "lib\\BGFX\\external\\Windows\\x64\\bgfx-shared-libDebug.dll";
+}
