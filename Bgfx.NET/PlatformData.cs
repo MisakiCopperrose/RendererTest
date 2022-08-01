e502c3c85bbc3d39b497285a356b1b7156f45f13
winand <winand1990@live.nl>
2022-08-01 15:19:48 +0200

    Updating old code part I

--- Bgfx.NET/PlatformData.cs
+++ Bgfx.NET/PlatformData.cs
@@ -1,46 +1,46 @@
-﻿namespace Bgfx.NET {
+﻿namespace Bgfx.NET;
+
+/// <summary>
+/// Contains platform-specific data used to hook into the bgfx library.
+/// </summary>
+public struct PlatformData {
     /// <summary>
-    /// Contains platform-specific data used to hook into the bgfx library.
+    /// EGL native display type.
     /// </summary>
-    public struct PlatformData {
-        /// <summary>
-        /// EGL native display type.
-        /// </summary>
-        public IntPtr DisplayType;
-
-        /// <summary>
-        /// Platform window handle.
-        /// </summary>
-        public IntPtr WindowHandle;
-
-        /// <summary>
-        /// Device context to use instead of letting the library create its own.
-        /// </summary>
-        public IntPtr Context;
-
-        /// <summary>
-        /// Backbuffer pointer to use instead of letting the library create its own.
-        /// </summary>
-        public IntPtr Backbuffer;
-
-        /// <summary>
-        /// Depth-stencil pointer to use instead of letting the library create its own.
-        /// </summary>
-        public IntPtr BackbufferDepthStencil;
-    }
+    public IntPtr DisplayType;
 
     /// <summary>
-    /// Exposes internal API data for interop scenarios.
+    /// Platform window handle.
     /// </summary>
-    public struct InternalData {
-        /// <summary>
-        /// Pointer to internal Bgfx capabilities structure. Use <see cref="Bgfx.GetCaps"/> instead.
-        /// </summary>
-        public IntPtr Caps;
+    public IntPtr WindowHandle;
 
-        /// <summary>
-        /// The underlying API's device context (OpenGL, Direct3D, etc).
-        /// </summary>
-        public IntPtr Context;
-    }
+    /// <summary>
+    /// Device context to use instead of letting the library create its own.
+    /// </summary>
+    public IntPtr Context;
+
+    /// <summary>
+    /// Backbuffer pointer to use instead of letting the library create its own.
+    /// </summary>
+    public IntPtr Backbuffer;
+
+    /// <summary>
+    /// Depth-stencil pointer to use instead of letting the library create its own.
+    /// </summary>
+    public IntPtr BackbufferDepthStencil;
+}
+
+/// <summary>
+/// Exposes internal API data for interop scenarios.
+/// </summary>
+public struct InternalData {
+    /// <summary>
+    /// Pointer to internal Bgfx capabilities structure. Use <see cref="Bgfx.GetCaps"/> instead.
+    /// </summary>
+    public IntPtr Caps;
+
+    /// <summary>
+    /// The underlying API's device context (OpenGL, Direct3D, etc).
+    /// </summary>
+    public IntPtr Context;
 }
--- Bgfx.NET/Shader.cs
+++ Bgfx.NET/Shader.cs
@@ -1,121 +1,121 @@
-﻿namespace Bgfx.NET {
+﻿namespace Bgfx.NET;
+
+/// <summary>
+/// Represents a single compiled shader component.
+/// </summary>
+public unsafe struct Shader : IDisposable, IEquatable<Shader> {
+    Uniform[] uniforms;
+    internal readonly ushort handle;
+
     /// <summary>
-    /// Represents a single compiled shader component.
+    /// Represents an invalid handle.
     /// </summary>
-    public unsafe struct Shader : IDisposable, IEquatable<Shader> {
-        Uniform[] uniforms;
-        internal readonly ushort handle;
+    public static readonly Shader Invalid = new Shader();
 
-        /// <summary>
-        /// Represents an invalid handle.
-        /// </summary>
-        public static readonly Shader Invalid = new Shader();
-
-        /// <summary>
-        /// The set of uniforms exposed by the shader.
-        /// </summary>
-        public IReadOnlyList<Uniform> Uniforms {
-            get {
-                if (uniforms == null) {
-                    var count = NativeMethods.bgfx_get_shader_uniforms(handle, null, 0);
-                    uniforms = new Uniform[count];
-                    NativeMethods.bgfx_get_shader_uniforms(handle, uniforms, count);
-                }
-
-                return uniforms;
+    /// <summary>
+    /// The set of uniforms exposed by the shader.
+    /// </summary>
+    public IReadOnlyList<Uniform> Uniforms {
+        get {
+            if (uniforms == null) {
+                var count = NativeMethods.bgfx_get_shader_uniforms(handle, null, 0);
+                uniforms = new Uniform[count];
+                NativeMethods.bgfx_get_shader_uniforms(handle, uniforms, count);
             }
-        }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Shader"/> struct.
-        /// </summary>
-        /// <param name="memory">The compiled shader memory.</param>
-        public Shader (MemoryBlock memory) {
-            handle = NativeMethods.bgfx_create_shader(memory.ptr);
-            uniforms = null;
+            return uniforms;
         }
+    }
 
-        /// <summary>
-        /// Releases the shader.
-        /// </summary>
-        public void Dispose () {
-            NativeMethods.bgfx_destroy_shader(handle);
-        }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Shader"/> struct.
+    /// </summary>
+    /// <param name="memory">The compiled shader memory.</param>
+    public Shader (MemoryBlock memory) {
+        handle = NativeMethods.bgfx_create_shader(memory.Ptr);
+        uniforms = null;
+    }
 
-        /// <summary>
-        /// Sets the name of the shader, for debug display purposes.
-        /// </summary>
-        /// <param name="name">The name of the shader.</param>
-        public void SetName(string name) {
-            NativeMethods.bgfx_set_shader_name(handle, name, int.MaxValue);
-        }
+    /// <summary>
+    /// Releases the shader.
+    /// </summary>
+    public void Dispose () {
+        NativeMethods.bgfx_destroy_shader(handle);
+    }
 
-        /// <summary>
-        /// Determines whether the specified object is equal to this instance.
-        /// </summary>
-        /// <param name="other">The object to compare with this instance.</param>
-        /// <returns><c>true</c> if the specified object is equal to this instance; otherwise, <c>false</c>.</returns>
-        public bool Equals (Shader other) {
-            return handle == other.handle;
-        }
+    /// <summary>
+    /// Sets the name of the shader, for debug display purposes.
+    /// </summary>
+    /// <param name="name">The name of the shader.</param>
+    public void SetName(string name) {
+        NativeMethods.bgfx_set_shader_name(handle, name, int.MaxValue);
+    }
 
-        /// <summary>
-        /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
-        /// </summary>
-        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
-        /// <returns>
-        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
-        /// </returns>
-        public override bool Equals (object obj) {
-            var other = obj as Shader?;
-            if (other == null)
-                return false;
+    /// <summary>
+    /// Determines whether the specified object is equal to this instance.
+    /// </summary>
+    /// <param name="other">The object to compare with this instance.</param>
+    /// <returns><c>true</c> if the specified object is equal to this instance; otherwise, <c>false</c>.</returns>
+    public bool Equals (Shader other) {
+        return handle == other.handle;
+    }
 
-            return Equals(other);
-        }
+    /// <summary>
+    /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+    /// </summary>
+    /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+    /// <returns>
+    ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+    /// </returns>
+    public override bool Equals (object obj) {
+        var other = obj as Shader?;
+        if (other == null)
+            return false;
 
-        /// <summary>
-        /// Returns a hash code for this instance.
-        /// </summary>
-        /// <returns>
-        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
-        /// </returns>
-        public override int GetHashCode () {
-            return handle.GetHashCode();
-        }
+        return Equals(other);
+    }
 
-        /// <summary>
-        /// Returns a <see cref="System.String"/> that represents this instance.
-        /// </summary>
-        /// <returns>
-        /// A <see cref="System.String"/> that represents this instance.
-        /// </returns>
-        public override string ToString () {
-            return string.Format("Handle: {0}", handle);
-        }
+    /// <summary>
+    /// Returns a hash code for this instance.
+    /// </summary>
+    /// <returns>
+    /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
+    /// </returns>
+    public override int GetHashCode () {
+        return handle.GetHashCode();
+    }
 
-        /// <summary>
-        /// Implements the equality operator.
-        /// </summary>
-        /// <param name="left">The left side of the operator.</param>
-        /// <param name="right">The right side of the operator.</param>
-        /// <returns>
-        /// <c>true</c> if the two objects are equal; otherwise, <c>false</c>.
-        /// </returns>
-        public static bool operator ==(Shader left, Shader right) {
-            return left.Equals(right);
-        }
+    /// <summary>
+    /// Returns a <see cref="System.String"/> that represents this instance.
+    /// </summary>
+    /// <returns>
+    /// A <see cref="System.String"/> that represents this instance.
+    /// </returns>
+    public override string ToString () {
+        return string.Format("Handle: {0}", handle);
+    }
 
-        /// <summary>
-        /// Implements the inequality operator.
-        /// </summary>
-        /// <param name="left">The left side of the operator.</param>
-        /// <param name="right">The right side of the operator.</param>
-        /// <returns>
-        /// <c>true</c> if the two objects are not equal; otherwise, <c>false</c>.
-        /// </returns>
-        public static bool operator !=(Shader left, Shader right) {
-            return !left.Equals(right);
-        }
+    /// <summary>
+    /// Implements the equality operator.
+    /// </summary>
+    /// <param name="left">The left side of the operator.</param>
+    /// <param name="right">The right side of the operator.</param>
+    /// <returns>
+    /// <c>true</c> if the two objects are equal; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool operator ==(Shader left, Shader right) {
+        return left.Equals(right);
+    }
+
+    /// <summary>
+    /// Implements the inequality operator.
+    /// </summary>
+    /// <param name="left">The left side of the operator.</param>
+    /// <param name="right">The right side of the operator.</param>
+    /// <returns>
+    /// <c>true</c> if the two objects are not equal; otherwise, <c>false</c>.
+    /// </returns>
+    public static bool operator !=(Shader left, Shader right) {
+        return !left.Equals(right);
     }
 }
